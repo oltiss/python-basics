4f2d843bb06af81c51e6d12dc783f26f0bbf0194
oltiss <czstolec@outlook.com>
2026-08-05 01:12:52 +0200

    incomplete function that adds income

--- 4.finance_manager/main.py
+++ 4.finance_manager/main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import subprocess
 
 
 db_name = "4.finance_manager/finances.db"
@@ -17,11 +18,31 @@ def init_db():
             id INTEGER PRIMARY KEY,
             amount INTEGER NOT NULL,
             category TEXT NOT NULL,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
         )
     """)
     conn.commit()
     conn.close()
+
+
+def add_income():
+
+    try:
+        conn, cur = _conn_to_db()
+        amount = input("Enter amount: ")
+        if amount >= 0:
+            print("You cannot enter negative or zero income!")
+        else:
+
+
+            query = "INSERT INTO finances (amount, category) VALUES(?, ?)"
+            cur.execute(query, (amount, category))
+
+    except ValueError:
+        print("Please enter a number!")
+
+
+
 
 
 def print_menu():
@@ -30,10 +51,37 @@ def print_menu():
     print("2. Add expense")
     print("3. Show balance")
     print("4. Show monthly expense analysis")
+    print("5. Manage categories")
+    print("5. Quit")
     print("===================================")
 
 def main():
-    print_menu()
+    while True:
+        subprocess.run(["clear"])
+        print_menu()
+
+        choice = input("Enter number from 1-5: ")
+
+        match choice:
+            case "1":
+                print("1")
+                input("Press enter...")
+            case "2":
+                print("2")
+                input("Press enter...")
+            case "3":
+                print("3")
+                input("Press enter...")
+            case "4":
+                print("4")
+                input("Press enter...")
+            case "5":
+                print("Thanks for using my finance manager!")
+                break
+            case _:
+                print("Please enter number from 1-5")
+                input("Press enter...")
+
 
 
 
